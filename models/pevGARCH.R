cc7d7edd3f7101e67c05596474a163e3cc584021
Juniper Simonis <dapperstats@gmail.com>
2019-11-07 04:00:33 -0500

    stack_testing

---
+++ models/pevGARCH.R
@@ -0,0 +1,6 @@
+cast_all_interp <- pevGARCH(data_set = "all_interp", lag = 6, main = ".", control_files = files_control(), quiet = FALSE, verbose = FALSE, arg_checks = TRUE);
+save_cast_output(cast_all_interp, main = ".", quiet = FALSE, arg_checks = TRUE);
+cast_controls_interp <- pevGARCH(data_set = "controls_interp", lag = 6, main = ".", control_files = files_control(), quiet = FALSE, verbose = FALSE, arg_checks = TRUE);
+save_cast_output(cast_controls_interp, main = ".", quiet = FALSE, arg_checks = TRUE);
+cast_exclosures_interp <- pevGARCH(data_set = "exclosures_interp", lag = 6, main = ".", control_files = files_control(), quiet = FALSE, verbose = FALSE, arg_checks = TRUE);
+save_cast_output(cast_exclosures_interp, main = ".", quiet = FALSE, arg_checks = TRUE);
