45af08c8ae37f5ef3e1fa85a28251f915d4c062e
Juniper Simonis <dapperstats@gmail.com>
2019-11-07 04:13:38 -0500

    stack_testing

---
+++ models/AutoArima.R
@@ -0,0 +1,6 @@
+cast_all <- AutoArima(data_set = "all", main = ".", control_files = files_control(), quiet = FALSE, verbose = FALSE, arg_checks = TRUE);
+save_cast_output(cast_all, main = ".", quiet = FALSE, arg_checks = TRUE);
+cast_controls <- AutoArima(data_set = "controls", main = ".", control_files = files_control(), quiet = FALSE, verbose = FALSE, arg_checks = TRUE);
+save_cast_output(cast_controls, main = ".", quiet = FALSE, arg_checks = TRUE);
+cast_exclosures <- AutoArima(data_set = "exclosures", main = ".", control_files = files_control(), quiet = FALSE, verbose = FALSE, arg_checks = TRUE);
+save_cast_output(cast_exclosures, main = ".", quiet = FALSE, arg_checks = TRUE);
