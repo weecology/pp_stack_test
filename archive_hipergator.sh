9299c7fda8945bae147e4f69269f0d7ac84aa425
DAPPERstats <dapperstats@gmail.com>
2019-11-06 18:09:23 -0800

    Update archive_hipergator.sh

--- archive_hipergator.sh
+++ archive_hipergator.sh
@@ -1,6 +1,7 @@
 git config --global user.email "weecologydeploy@weecology.org"
 git config --global user.name "Weecology Deploy Bot"
 
+git checkout master
 git add casts/* data/* models/*
 git commit -m "trying to push"
 git push master
