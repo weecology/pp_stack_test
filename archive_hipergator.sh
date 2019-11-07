be1db58a4cd6b949693e0dfb97debbf6dce74057
DAPPERstats <dapperstats@gmail.com>
2019-11-07 00:05:22 -0800

    Update archive_hipergator.sh

--- archive_hipergator.sh
+++ archive_hipergator.sh
@@ -5,5 +5,5 @@ git checkout master
 git add casts/* 
 git commit -m "trying to push"
 
-git remote add deploy https://juniperlsimonis@github.com/weecology/pp_stack_test.git > /dev/null 2>&1
+git remote add deploy https://${GITHUB_TOKEN}@github.com/weecology/pp_stack_test.git > /dev/null 2>&1
 git push --quiet deploy master > /dev/null 2>&1
