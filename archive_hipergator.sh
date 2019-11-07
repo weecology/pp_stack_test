18e9b95cd85722fe0244fa3133c08a2fb6c46343
DAPPERstats <dapperstats@gmail.com>
2019-11-07 00:57:06 -0800

    ok

--- archive_hipergator.sh
+++ archive_hipergator.sh
@@ -3,7 +3,12 @@ git config --global user.name "Juniper Simonis"
 
 git checkout master
 git add casts/* data/* models/*
-git commit -m "trying to push"
+git commit -m "stack_testing"
 
 git remote add deploy https://${GITHUB_TOKEN}@github.com/weecology/pp_stack_test.git > /dev/null 2>&1
+
+current_date=`date -I | head -c 10`
+git tag $current_date
+
 git push --quiet deploy master > /dev/null 2>&1
+git push --quiet deploy --tags > /dev/null 2>&1
