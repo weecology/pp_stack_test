47740c0d7bb69a14304d41f07830343b9636bf38
DAPPERstats <dapperstats@gmail.com>
2019-11-06 18:20:55 -0800

    Update archive_hipergator.sh

--- archive_hipergator.sh
+++ archive_hipergator.sh
@@ -4,3 +4,5 @@ git config --global user.name "Weecology Deploy Bot"
 git checkout master
 git add casts/* data/* models/*
 git commit -m "trying to push"
+git remote add deploy https://${GITHUB_TOKEN}@github.com/weecology/portalPredictions.git > /dev/null 2>&1
+git push --quiet deploy master > /dev/null 2>&1
