9c70eea8b328b839d5dc1e48e4ba49d345594de4
DAPPERstats <dapperstats@gmail.com>
2019-11-07 00:47:53 -0800

    trying

--- archive_hipergator.sh
+++ archive_hipergator.sh
@@ -2,7 +2,7 @@ git config --global user.email "dapperstats@gmail.com"
 git config --global user.name "Juniper Simonis"
 
 git checkout master
-git add casts/* 
+git add casts/* data/* models/*
 git commit -m "trying to push"
 
 git remote add deploy https://${GITHUB_TOKEN}@github.com/weecology/pp_stack_test.git > /dev/null 2>&1
--- stack_test.R
+++ stack_test.R
@@ -1,5 +1,2 @@
 library(portalcasting)
-#setup_dir()
-dir.create("casts")
-a <- "a"
-save(a, file = "casts/a.RData")
+setup_dir()
