213210f7a2c893b525901dbaac1d22369aea0533
DAPPERstats <dapperstats@gmail.com>
2019-11-07 01:04:04 -0800

    yes

--- archive_hipergator.sh
+++ archive_hipergator.sh
@@ -12,3 +12,4 @@ git tag $current_date
 
 git push --quiet deploy master > /dev/null 2>&1
 git push --quiet deploy --tags > /dev/null 2>&1
+curl -v -i -X POST -H "Content-Type:application/json" -H "Authorization: token $GITHUB_RELEASE_TOKEN" https://api.github.com/repos/weecology/pp_stack_test/releases -d "{\"tag_name\":\"$current_date\"}"
